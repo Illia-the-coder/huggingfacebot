4b41267707aeca5d4c1eaa8e665d1ae9c00b4e2b
illia Liudohovskyi <101904816+Illia-the-coder@users.noreply.github.com>
2024-08-01 11:23:55 +0200

    _

--- huggingface/spiders/hf_spider.py
+++ huggingface/spiders/hf_spider.py
@@ -7,7 +7,7 @@ class HfSpiderSpider(scrapy.Spider):
     start_urls = ["https://huggingface.co"]
 
     def start_requests(self):
-        # Відкриваємо файл з URL профілів
+        # URL to the text file containing profile URLs
         url = 'https://raw.githubusercontent.com/Illia-the-coder/huggingfacebot/master/huggingface/spiders/hf_profiles_urls.txt'
         page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
         urls = page.text.splitlines()
@@ -17,26 +17,25 @@ class HfSpiderSpider(scrapy.Spider):
     def parse(self, response):
         def extract_with_css(query):
             return response.css(query).get(default='').strip()
+
         def extract_with_xpath(query):
             return response.xpath(query).get(default='').strip()
+
         def extract_with_xpath_length(query):
             return len(response.xpath(query).getall())
-
-        
-
         yield {
-            'Ім\'я користувача': extract_with_xpath('/html/body/div/main/div/div/section[1]/h1/span/text()'),
-            'Нікнейм': extract_with_css('div.mb-4.inline-block.rounded::text'),
-            'Посилання на X': extract_with_css('a[href*="twitter.com"]::attr(href)'),
-            'Посилання на GitHub': extract_with_css('a[href*="github.com"]::attr(href)'),
-            'Посилання на LinkedIn': extract_with_css('a[href*="linkedin.com"]::attr(href)'),
-            'Посилання на сайт': extract_with_xpath('/html/body/div/main/div/div/section[1]/div[5]/div/a/@href'),
-            'Інтереси': extract_with_xpath('/html/body/div/main/div/div/section[1]/div[6]/text()') ,
-            'Кількість статей': extract_with_xpath_length('/html/body/div/main/div/div/section[1]/div[7]/div/div[1]/div'),
-            'Кількість моделей': int(extract_with_xpath('//*[@id="models"]/h3/div[1]/span[2]/text()') or 0),
-            'Кількість спейсів': int(extract_with_xpath( '//*[@id="spaces"]/h3/div[1]/span[2]/text()') or 0),
-            'Кількість датасетів': int(extract_with_xpath('///*[@id="datasets"]/h3/div[1]/span[2]/text()') or 0),
-            'Кількість організацій': extract_with_xpath_length('/html/body/div/main/div/div/section[1]/div[8]/a/img'),
-            'Кількість людей, які підписані': int(extract_with_xpath('/html/body/div/main/div/div/section[1]/div[4]/button[1]/text()').split('\n')[0]),
-            'Кількість людей на яких підписаний': int(extract_with_xpath('/html/body/div/main/div/div/section[1]/div[4]/button[2]/text()').split(' ')[0]),
+            'username': extract_with_xpath('/html/body/div/main/div/div/section[1]/h1/span/text()'),
+            'nickname': extract_with_css('div.mb-4.inline-block.rounded::text'),
+            'twitter_link': extract_with_css('a[href*="twitter.com"]::attr(href)'),
+            'github_link': extract_with_css('a[href*="github.com"]::attr(href)'),
+            'linkedin_link': extract_with_css('a[href*="linkedin.com"]::attr(href)'),
+            'website_link': extract_with_xpath('/html/body/div/main/div/div/section[1]/div[5]/div/a/@href'),
+            'interests': extract_with_xpath('/html/body/div/main/div/div/section[1]/div[6]/text()'),
+            'articles_count': extract_with_xpath_length('/html/body/div/main/div/div/section[1]/div[7]/div/div[1]/div'),
+            'models_count': int(extract_with_xpath('//*[@id="models"]/h3/div[1]/span[2]/text()') or 0),
+            'spaces_count': int(extract_with_xpath('//*[@id="spaces"]/h3/div[1]/span[2]/text()') or 0),
+            'datasets_count': int(extract_with_xpath('//*[@id="datasets"]/h3/div[1]/span[2]/text()') or 0),
+            'organizations_count': extract_with_xpath_length('/html/body/div/main/div/div/section[1]/div[7]/a/img'),
+            'followers_count': int(extract_with_xpath('/html/body/div/main/div/div/section[1]/div[4]/button[1]/text()').split('\n')[0]),
+            'following_count': int(extract_with_xpath('/html/body/div/main/div/div/section[1]/div[4]/button[2]/text()').split(' ')[0]),
         }
